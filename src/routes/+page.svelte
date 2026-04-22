403380bac20fdc06e96f9ebca4ffb710c9b0f8ef
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2026-04-22 19:40:09 -0400

    Revamp home page hero and apply dark theme
    Replace home page with new profile hero and CTAs
    Apply dark theme with black bg and white text
    Update Tailwind import to use double quotes in layout.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="max-w-xl mx-auto text-center">
+    <div class="w-fit mx-auto">
+        <img
+            class="rounded-full w-24 h-24 mx-auto border-2 border-[#262626]"
+            src="https://selfhosted.social/xrpc/com.atproto.sync.getBlob?did=did:plc:kal7cndv3fyeyo3qw3ejixmd&cid=bafkreifwlccr5hlzq6v4az7tlibhr4aqq3xckuifejmkgwxejjysnudpue"
+            alt="Isaiah Hamilton"
+        />
+        <div
+            class="-translate-y-4 flex items-center gap-2 px-2 py-1 w-fit mx-auto rounded-full bg-[#0E0E0E]"
+        >
+            <div class="relative h-2 w-2">
+                <div
+                    class="absolute animate-ping bg-green-500 h-full w-full rounded-full"
+                ></div>
+                <div
+                    class="absolute bg-green-500 h-full w-full rounded-full"
+                ></div>
+            </div>
+            <p class="text-sm font-medium">Available for work</p>
+        </div>
+    </div>
+    <div class="space-y-4">
+        <h1 class="text-6xl font-semibold">
+            I design and build modern experiences
+        </h1>
+        <p class="text-base text-[#86868b] max-w-sm mx-auto">
+            Hey, I'm Isaiah. I design and build modern digital experiences for
+            individuals and growing businesses.
+        </p>
+    </div>
+    <div class="flex items-center gap-2 w-fit mx-auto mt-8">
+        <a href="">
+            <div
+                class="transition duration-300 flex items-center h-10 w-30 rounded-full bg-[#0E0E0E] shadow-[0px_0px_0px_0px_#262626] hover:shadow-[0px_0px_0px_3px_#262626]"
+            >
+                <p class="text-sm font-medium mx-auto">See my work</p>
+            </div>
+        </a>
+        <a href="">
+            <div
+                class="transition duration-300 flex items-center h-10 w-30 rounded-full bg-white text-black hover:bg-blue-500 hover:text-white"
+            >
+                <p class="text-sm font-medium mx-auto">Contact me</p>
+            </div>
+        </a>
+    </div>
+</div>
