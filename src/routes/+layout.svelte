db5c63eb8ba80ae99b7b96eb7fb1f59b44379042
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2026-04-24 10:47:22 -0400

    Add fixed navigation bar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="relative">
+    <nav
+        class="fixed top-4 left-1/2 transform -translate-x-1/2 z-20 w-fit mx-auto border border-[#262626] rounded-full px-4 py-2 backdrop-blur-md bg-[#121212b3]"
+    >
+        <ul
+            class="flex items-center justify-center gap-4 text-[#86868b] text-sm"
+        >
+            <li
+                class="transition-colors duration-500 ease-out hover:text-[#ffffff]"
+            >
+                <a href="/">Services</a>
+            </li>
+            <li
+                class="transition-colors duration-500 ease-out hover:text-[#ffffff]"
+            >
+                <a href="/about">Works</a>
+            </li>
+            <li
+                class="transition-colors duration-500 ease-out hover:text-[#ffffff]"
+            >
+                <a href="/contact">Contact me</a>
+            </li>
+        </ul>
+    </nav>
+    <main class="container mx-auto">
+        {@render children()}
+    </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,43 @@
-<div class="max-w-xl mx-auto text-center">
+<script lang="ts">
+    const services = [
+        {
+            icon: "paint brush",
+            title: "Branding & Identity",
+            description:
+                "Creating logos, visual styles, and entire brand identities that help your business stand out.",
+        },
+        {
+            icon: "paint brush",
+            title: "Branding & Identity",
+            description:
+                "Creating logos, visual styles, and entire brand identities that help your business stand out.",
+        },
+        {
+            icon: "paint brush",
+            title: "Branding & Identity",
+            description:
+                "Creating logos, visual styles, and entire brand identities that help your business stand out.",
+        },
+        {
+            icon: "paint brush",
+            title: "Branding & Identity",
+            description:
+                "Creating logos, visual styles, and entire brand identities that help your business stand out.",
+        },
+    ];
+
+    const works = [
+        {
+            title: "Violet",
+            description: "Framer Template",
+            image: "https://framerusercontent.com/images/7VybckhbrXvFd1uHTiacrudPcXM.png?width=1000&height=750",
+        },
+    ];
+</script>
+
+<div
+    class="h-screen flex flex-col items-center justify-center max-w-xl mx-auto text-center"
+>
     <div class="w-fit mx-auto">
         <img
             class="rounded-full w-24 h-24 mx-auto border-2 border-[#262626]"
@@ -20,7 +59,7 @@
         </div>
     </div>
     <div class="space-y-4">
-        <h1 class="text-6xl font-semibold">
+        <h1 class="text-6xl font-semibold leading-tight">
             I design and build modern experiences
         </h1>
         <p class="text-base text-[#86868b] max-w-sm mx-auto">
@@ -43,5 +82,61 @@
                 <p class="text-sm font-medium mx-auto">Contact me</p>
             </div>
         </a>
+    </div>
+</div>
+
+<div class="mt-40 space-y-14">
+    <div class="text-center">
+        <p class="text-sm font-semibold text-[#86868b]">Services</p>
+        <h2 class="text-5xl font-semibold leading-tight mt-6 max-w-110 mx-auto">
+            Here's what I bring to the table for you
+        </h2>
+        <p class="text-[#86868b] mt-4 max-w-xs mx-auto">
+            An overview of the core services I provide, no matter the project
+            size or type.
+        </p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-200 mx-auto">
+        {#each services as service}
+            <div
+                class="space-y-6 bg-[#0e0e0e] border-2 border-[#262626] rounded-2xl px-8 py-6"
+            >
+                <div>{service.icon}</div>
+                <div class="space-y-2">
+                    <h3 class="text-2xl font-semibold">{service.title}</h3>
+                    <p class="text-base font-medium text-[#86868b]">
+                        {service.description}
+                    </p>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
+
+<div class="mt-40 space-y-14">
+    <div class="text-center">
+        <p class="text-sm font-semibold text-[#86868b]">Works</p>
+        <h2 class="text-5xl font-semibold leading-tight mt-6 max-w-110 mx-auto">
+            Take a closer look at some of my works
+        </h2>
+        <p class="text-[#86868b] mt-4 max-w-xs mx-auto">
+            A selection of projects that showcase my approach, style, and
+            attention to detail.
+        </p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-200 mx-auto">
+        {#each works as work}
+            <div
+                class="space-y-2 bg-[#0e0e0e] border-2 border-[#262626] rounded-2xl px-2 py-2"
+            >
+                <img src={work.image} alt={work.title} class="rounded-xl" />
+                <div class="space-y-2">
+                    <h3 class="text-base font-medium">{work.title}</h3>
+                    <p class="text-sm font-semibold text-[#86868b]">
+                        {work.description}
+                    </p>
+                </div>
+            </div>
+        {/each}
     </div>
 </div>
