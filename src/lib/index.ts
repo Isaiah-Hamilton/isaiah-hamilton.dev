8e777e784cda5083194088cb62398582ab1f2f2d
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2026-05-08 22:50:50 -0400

    export sleep as constant and remove old function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,8 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 export enum Theme {
   Dark = "dark",
   Light = "light",
   System = "system",
   RealTime = "realtime",
-}
-
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
