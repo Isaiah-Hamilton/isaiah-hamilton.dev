7e013119cd9e8cb8cf1485bc88070b1c688b515e
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2026-05-14 02:09:17 -0400

    initialize theme on mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,19 @@
     import "./layout.css";
     import Footer from "$lib/components/footer.svelte";
     import Navbar from "$lib/components/navbar.svelte";
-    import ThemeProvider from "$lib/theme/provider.svelte";
+    import { theme } from "$lib/theme/store.svelte";
+    import { onMount } from "svelte";
 
     let { children } = $props();
+    let mounted = $state(false);
+
+    onMount(() => {
+        theme.init();
+        mounted = true;
+    });
 </script>
 
-<ThemeProvider>
+{#if mounted}
     <div class="relative">
         <Navbar />
         <main class="overflow-x-hidden">
@@ -15,4 +22,4 @@
         </main>
         <Footer />
     </div>
-</ThemeProvider>
+{/if}
